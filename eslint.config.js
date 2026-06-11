a216df56f43447bb30e50ec71c1f71cde64778ef
HoroOrangeWolf <wojciech.minner@gmail.com>
2026-06-11 21:52:32 +0200

    Added eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import {defineConfig, globalIgnores} from 'eslint/config'
+import react from 'eslint-plugin-react';
+import preferArrow from "eslint-plugin-prefer-arrow";
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default defineConfig([
+    globalIgnores(['dist']),
+    ...eslintPluginAstro.configs.recommended,
+    {
+        plugins: {
+            react,
+            unicorn: eslintPluginUnicorn,
+            "prefer-arrow": preferArrow,
+        },
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            react.configs.flat.recommended,
+            react.configs.flat['jsx-runtime'],
+            eslintPluginUnicorn.configs.recommended,
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        rules: {
+            "object-curly-spacing": ["error", "always"],
+            "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
+            "react/jsx-curly-brace-presence": ["error", { "props": "never", "children": "never" }],
+            "unicorn/no-null": ["off"],
+            "unicorn/prefer-spread": ['off'],
+            "unicorn/filename-case": ['off'],
+            "react/jsx-max-props-per-line": ["error", { "maximum": 1, "when": "multiline" }],
+            "react/jsx-first-prop-new-line": ["error", "multiline"],
+            "semi": ["error", "always"],
+            "array-bracket-spacing": ["error", "never"],
+            "react/jsx-tag-spacing": ["error", { "beforeSelfClosing": "always" }],
+            "indent": ["error", "tab", { "SwitchCase": 1 }],
+            "react/jsx-indent": ["error", "tab"],
+            "react/jsx-indent-props": ["error", "tab"],
+            "@typescript-eslint/no-explicit-any": ["off"],
+            "unicorn/prevent-abbreviations": ["off"],
+            "react-refresh/only-export-components": ["off"],
+
+            "prefer-arrow/prefer-arrow-functions": [
+                "error",
+                {
+                    "disallowPrototype": true,
+                    "singleReturnOnly": true,
+                    "classPropertiesAllowed": false
+                }
+            ]
+        },
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            }
+        },
+    },
+])
