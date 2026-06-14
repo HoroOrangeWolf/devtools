a8b7355bdaad355474fb9ba1e5bc27c97df7fa34
HoroOrangeWolf <wojciech.minner@gmail.com>
2026-06-14 18:14:36 +0200

    Exported site to different file to automatically create proper layout.

---
+++ src/constant/sites.contant.ts
@@ -0,0 +1,26 @@
+export const NavItems = [
+	{
+		href: '/uuid',
+		label: 'UUID',
+		description: 'Generate UUID values',
+		isReady: true,
+	},
+	{
+		href: '/csv',
+		label: 'CSV',
+		description: 'Format and export data',
+		isReady: true,
+	},
+	{
+		href: '#',
+		label: 'JSON',
+		description: 'Inspect and transform JSON',
+		isReady: false,
+	},
+	{
+		href: '#',
+		label: 'Hash',
+		description: 'Create checksums',
+		isReady: false,
+	},
+] as const;
