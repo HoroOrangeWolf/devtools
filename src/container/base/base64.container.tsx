9cfa3a6477847004f1ba9fa4b83ec53e12aa6770
HoroOrangeWolf <wojciech.minner@gmail.com>
2026-06-15 21:37:14 +0200

    Added working encoder and decoder

--- src/container/base/base64.container.tsx
+++ src/container/base/base64.container.tsx
@@ -1,10 +1,13 @@
 import { cn } from '@/lib/utils.ts';
 import { Textarea } from '@/components/ui/textarea.tsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { OptionType, SelectWrapper } from '@/components/selectWrapper.component.tsx';
 import { BaseVariant, BaseVariantConstant } from '@/service/constant/baseVariant.constant.ts';
 import { Button } from '@/components/ui/button';
 import { ButtonGroup } from '@/components/ui/button-group.tsx';
+import { Field, FieldLabel } from '@/components/ui/field.tsx';
+import { Label } from 'radix-ui';
+import { BaseService } from '@/service/base.service.ts';
 
 const BASE_VARIANTS: OptionType<BaseVariant>[] = [
 	{
@@ -13,13 +16,35 @@ const BASE_VARIANTS: OptionType<BaseVariant>[] = [
 	},
 	{
 		label: 'Url-safe Base64',
-		value: BaseVariantConstant.BASE_64
+		value: BaseVariantConstant.BASE_64_URL
 	}
 ];
 
 export const Base64Container = () => {
+	const [sourceText, setSourceText] = useState<string>('');
+	const [targetText, setTargetText] = useState<string>('');
 	const [isEncodeMode, setIsEncodeMode] = useState<boolean>(true);
 	const [baseVariant, setBaseVariant] = useState<BaseVariant>(BaseVariantConstant.BASE_64);
+	const [isEncodingError, setIsEncodingError] = useState<boolean>(false);
+
+	useEffect(() => {
+		const fn = async () => {
+			try {
+				const method = isEncodeMode ? BaseService.encode : BaseService.decode;
+
+				const result = method(sourceText, baseVariant);
+
+				setTargetText(result);
+				setIsEncodingError(false);
+			} catch (error) {
+				console.error('Failed to',error);
+				setIsEncodingError(true);
+			}
+		};
+
+		fn()
+			.catch(console.error);
+	}, [isEncodeMode, baseVariant, sourceText]);
 
 	const switchEncodeMode = () => {
 		setIsEncodeMode(true);
@@ -31,15 +56,24 @@ export const Base64Container = () => {
 
 	return (
 		<div className={cn('flex flex-col gap-2')}>
-			<SelectWrapper
-				options={BASE_VARIANTS}
-				defaultValue={baseVariant}
-				onChange={setBaseVariant}
-			/>
+			<div className={cn('w-48')}>
+				<Field>
+					<FieldLabel htmlFor="base_variant">
+						Base64 Variant
+					</FieldLabel>
+					<SelectWrapper
+						id="base_variant"
+						options={BASE_VARIANTS}
+						defaultValue={baseVariant}
+						onChange={setBaseVariant}
+					/>
+				</Field>
+			</div>
 			<div className={cn('flex flex-col gap-2')}>
 				<Textarea
 					className={cn('h-64')}
-					placeholder=""
+					onChange={(e)=>setSourceText(e.target.value)}
+					placeholder={`Type here value to ${isEncodeMode ? 'encode' : 'decode'}...`}
 				/>
 				<div className={cn('flex flex-row')}>
 					<ButtonGroup>
@@ -48,7 +82,8 @@ export const Base64Container = () => {
 					</ButtonGroup>
 				</div>
 				<Textarea
-					placeholder=""
+					placeholder={isEncodeMode ? 'Encoding results...' : 'Decoding results...'}
+					value={targetText}
 					readOnly={true}
 					className={cn('h-64')}
 				/>
