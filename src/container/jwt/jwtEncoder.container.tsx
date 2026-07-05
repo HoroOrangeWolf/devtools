d883b78e683d30e5b07cbb18083cbfcce3182ad7
HoroOrangeWolf <wojciech.minner@gmail.com>
2026-07-05 21:11:42 +0200

    Created base jwt encoder.

--- src/container/jwt/jwtEncoder.container.tsx
+++ src/container/jwt/jwtEncoder.container.tsx
@@ -1,9 +1,96 @@
+import { Field, FieldContent, FieldLabel } from '@/components/ui/field.tsx';
+import { JsonEditorContainer } from '@/container/json/jsonEditor.container.tsx';
+import { ViewTypeConstant } from '@/container/json/constant/viewType.constant.ts';
+import { Textarea } from '@/components/ui/textarea.tsx';
+import { useEffect, useState } from 'react';
+import { JwtService } from '@/container/jwt/service/jwt.service.ts';
+import { BannerComponent } from '@/components/banner.component.tsx';
 
 export const JwtEncoderContainer = () => {
+	const [header, setHeader] = useState<string>('{"alg": "HS256"}');
+	const [payload, setPayload] = useState<string>('{}');
+	const [secret, setSecret] = useState<string>('');
+	const [result, setResult] = useState<string>('');
+	const [errorMessage, setErrorMessage] = useState<string>();
+
+	useEffect(() => {
+		const fn = async () => {
+			try {
+				setErrorMessage(undefined);
+				const signedJwt = await JwtService.signJWT(header, payload, secret);
+				setResult(signedJwt);
+			} catch (error) {
+				console.error('Failed to sign key',error);
+				setErrorMessage('Failed to sign key : ' + (error as Error)?.message);
+			}
+		};
+
+		fn()
+			.catch(console.error);
+	}, [header, payload, secret]);
 
 	return (
-		<div>
-			Test
+		<div className="flex flex-col gap-2">
+			<div className="grid grid-cols-2 gap-2">
+				<div className="grid grid-rows-[repeat(3,minmax(15rem,1fr))] gap-2">
+					<Field className="h-full">
+						<FieldLabel>
+							JWT Header
+						</FieldLabel>
+						<FieldContent className="h-full">
+							<JsonEditorContainer
+								className="h-full"
+								value={header}
+								onChange={setHeader}
+								defaultViewType={ViewTypeConstant.TREE}
+							/>
+						</FieldContent>
+					</Field>
+					<Field
+						className="h-full"
+					>
+						<FieldLabel>
+							JWT Payload
+						</FieldLabel>
+						<FieldContent
+							className="h-full"
+						>
+							<JsonEditorContainer
+								className="h-full"
+								value={payload}
+								onChange={setPayload}
+								defaultViewType={ViewTypeConstant.TREE}
+							/>
+						</FieldContent>
+					</Field>
+					<Field
+						className="h-full"
+					>
+						<FieldLabel>Secret</FieldLabel>
+						<FieldContent
+							className="h-full"
+						>
+							<Textarea
+								className="h-full"
+								onChange={(e) => setSecret(e.target.value)}
+								value={secret}
+							/>
+						</FieldContent>
+					</Field>
+				</div>
+				<Textarea
+					value={result}
+					className="h-full"
+					placeholder="Signed JWT"
+					readOnly={true}
+				/>
+			</div>
+			<BannerComponent
+				variant={errorMessage ? 'destructive' : 'default'}
+				title={errorMessage ? 'Error' : 'Success'}
+			>
+				{errorMessage ?? 'JWT has been signed'}
+			</BannerComponent>
 		</div>
 	);
 };
